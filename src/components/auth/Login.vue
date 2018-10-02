0ed1f5410e1b433cd7e61713424b6a0e1d6dcf23
Drillan767 <2Gr@v10s+H6>
2018-10-02 17:38:50 +0200

    Login form is working, but nothing is handled.

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="login">
-    <form class="form-signin text-center">
+    <form class="form-signin text-center" @submit.prevent="signin">
       <img class="mb-4" src="https://placekitten.com/72/72" alt="" width="72" height="72">
       <h3 class="font-weight-normal">Please sign in</h3>
       <label for="inputEmail" class="sr-only">Email address</label>
-      <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+      <input type="email" v-model="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
       <label for="inputPassword" class="sr-only">Password</label>
-      <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+      <input type="password" v-model="password" id="inputPassword" class="form-control" placeholder="Password" required>
       <div class="checkbox mb-3">
         <label>
-          <input type="checkbox" value="remember-me"> Remember me
+          <input type="checkbox" value="remember-me" v-model="remember_me"> Remember me
         </label>
       </div>
       <button type="submit" class="btn btn-lg btn-block btn-outline-primary">Login</button>
@@ -19,22 +19,32 @@
 
 <script>
 export default {
+
+  data () {
+    return {
+      email: '',
+      password: '',
+      remember_me: 0,
+      error: []
+    }
+  },
+
   created () {
     this.$axios.get('/logged_in')
       .then(response => {
         if (response.data) {
-          this.$router.replace({ name: 'admin', params: { error: 'already_loggedin' } })
+          this.$router.replace({ name: 'admin' })
         }
       })
-    console.log(this.$router.params)
   },
 
-  watch: {
-    $route (to, from) {
-      console.log(to)
-      console.log(from)
+  methods: {
+    signin () {
+      this.$axios.post('/login', {email: this.email, password: this.password, remember_me: this.remember_me})
+        .then(response => console.log(response))
     }
   }
+
 }
 </script>
 
