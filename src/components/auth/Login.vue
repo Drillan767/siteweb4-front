e19390b80f649b37dc8104a08113d88a50862cb0
Drillan767 <2Gr@v10s+H6>
2018-10-03 09:55:30 +0200

    Issue with login form, will come back with the solution

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -24,7 +24,6 @@ export default {
     return {
       email: '',
       password: '',
-      remember_me: 0,
       error: []
     }
   },
@@ -40,43 +39,14 @@ export default {
 
   methods: {
     signin () {
-      this.$axios.post('/login', {email: this.email, password: this.password, remember_me: this.remember_me})
-        .then(response => console.log(response))
+      this.$axios.post('/login', {email: this.email, password: this.password})
+        .then(response => {
+          if (response.status === 200) {
+            this.$router.replace({ name: 'admin' })
+          }
+        })
     }
   }
 
 }
 </script>
-
-<style>
-  .hero.is-success {
-    background: #F2F6FA;
-  }
-  .hero .nav, .hero.is-success .nav {
-    -webkit-box-shadow: none;
-    box-shadow: none;
-  }
-  .box {
-    margin-top: 5rem;
-  }
-  .avatar {
-    margin-top: -70px;
-    padding-bottom: 20px;
-  }
-  .avatar img {
-    padding: 5px;
-    background: #fff;
-    border-radius: 50%;
-    -webkit-box-shadow: 0 2px 3px rgba(10,10,10,.1), 0 0 0 1px rgba(10,10,10,.1);
-    box-shadow: 0 2px 3px rgba(10,10,10,.1), 0 0 0 1px rgba(10,10,10,.1);
-  }
-  input {
-    font-weight: 300;
-  }
-  p {
-    font-weight: 700;
-  }
-  p.subtitle {
-    padding-top: 1rem;
-  }
-</style>
